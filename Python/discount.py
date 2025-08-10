43129f299e449ba2300ef3460e1929af502f585c
Chris Powell <blaine0406@gmail.com>
2025-08-09 22:28:12 -0400

    New Function - calculate_discounted_price(), main()
    created a new function called calculate_discounted_price() to do the math to get the final total. Added the main() function to handle the logic. Will work on added sys.argv[1] and [2]

--- Python/discount.py
+++ Python/discount.py
@@ -8,7 +8,18 @@ def get_float(prompt):
         except ValueError:
             print("Not a valid number!")
 
-amount = get_float("Enter the amount: ")
 
-print(amount)
+def calculate_discounted_price(amount, discount):
+    return round(amount * (1 - discount / 100), 2)
+
+def main():
+    amount = get_float("Enter the amount: $")
+    discount = get_float("Enter the discount %: ")
+    total = calculate_discounted_price(amount, discount)
+    print(f"Final Total: ${total}")
+
+if __name__ == "__main__":
+    main()
+
+
 
