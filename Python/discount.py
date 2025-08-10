cdab248b0f991cbce1d9d82b8ab58f376908151a
Chris Powell <blaine0406@gmail.com>
2025-08-09 22:14:54 -0400

    New Function
    Added get_float() to safely get valid nuumber from user as a float

--- Python/discount.py
+++ Python/discount.py
@@ -1,6 +1,14 @@
 import sys
 
-number = input("Enter a number: ")
 
-print(number)
+def get_float(prompt):
+    while True:
+        try:
+            return float(input(prompt))
+        except ValueError:
+            print("Not a valid number!")
+
+amount = get_float("Enter the amount: ")
+
+print(amount)
 
